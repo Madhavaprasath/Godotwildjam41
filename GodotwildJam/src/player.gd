36f85737c544c07b1906fd34c08f13a28a8072d6
Madhava <Madhavprasath088@gmail.com>
2022-01-16 23:20:30 +0530

    Movement

--- GodotwildJam/src/player.gd
+++ GodotwildJam/src/player.gd
@@ -1,16 +1,42 @@
-extends Node
+extends KinematicBody2D
+
+export(int) var speed=500
+
+enum states{
+	Idle,
+	Run,
+	Attack,
+	Aim,
+	Harpoon
+}
+var direction_vector=Vector2()
+var current_state=states.Idle
+var velocity=Vector2()
+
+func _ready():
+	pass
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+func _physics_process(delta):
+	handle_input(delta)
+	
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func handle_input(delta):
+	var direction={
+		"Left":Input.is_action_pressed("ui_left"),
+		"Right":Input.is_action_pressed("ui_right"),
+		"Up":Input.is_action_pressed("ui_up"),
+		"Down":Input.is_action_pressed("ui_down")
+	}
+	direction_vector.x=-int(direction["Left"])+int(direction["Right"])
+	direction_vector.y=int(direction["Down"])-int(direction["Up"])
+	
+	
+	if (direction_vector.y!=0 && direction_vector.x!=0):
+		direction_vector=Vector2()
+		velocity=Vector2()
+	else:
+		velocity=lerp(velocity,direction_vector*speed,1-pow(0.001,delta))
+	if(direction_vector==Vector2()):
+		velocity.move_toward(Vector2(),500*delta)
+	velocity=move_and_slide(velocity)
